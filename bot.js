b16085e953c36b2ba74bd5b4034e3884373dc84b
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2018-12-06 12:55:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', async message => {
-    
-    let args = message.content.split(' ').slice(1);
-if (message.content.startsWith("$$delete colors")) {
-if(!message.member.hasPermission('ADMINISTRATOR')) return
-let role = message.guild.roles.find('name', '1');
-
-role.delete()
-}
-
-});
+client.on('message', omar => { var prefix = "#"; if(omar.content.split(' ')[0] == prefix + 'dc') {  delete all channels if (!omar.channel.guild) return; if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**"); if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**"); omar.guild.channels.forEach(m => { m.delete(); }); } if(omar.content.split(' ')[0] == prefix + 'dr') {  delete all roles if (!omar.channel.guild) return; if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**"); if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**I Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**"); omar.guild.roles.forEach(m => { m.delete(); }); omar.reply("`تم حذف جميع الرتب بنجاح`") } });
 
 client.login(process.env.BOT_TOKEN);
